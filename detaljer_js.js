3ddefbca7126e1974952075fe6e1a5252f6c2f55
Caroline Juhl <caroliner.juhl@MBPtilhCaroline.lan>
2021-02-23 12:57:12 +0100

    ændringer i detajler

--- detaljer_js.js
+++ detaljer_js.js
@@ -16,18 +16,17 @@ console.log("ID", id);
 //Lytter om DOM er loaded inden data hentes
 document.addEventListener("DOMContentLoaded", hentData);
 
-//Henter data fra restdb
+//Henter data fra restdb og kalder funktion visSang
 async function hentData() {
-    console.log("Hent data");
+    console.log("Data hentes");
     const respons = await fetch(`https://faellespassion-9f07.restdb.io/rest/faellespassion/${id}`, options);
     sang = await respons.json();
-
-    console.log("Sange", sang);
     visSang(sang);
 }
 
+//Funktion der indsætter data fra restdb i HTML'en
 function visSang() {
-    console.log(id);
+    console.log("Sange", sang);
     document.querySelector(".billede").src = medieurl + sang.profilbillede;
     document.querySelector(".titel").textContent = sang.titel;
     document.querySelector(".kunstner").textContent = sang.band;
@@ -35,9 +34,10 @@ function visSang() {
     document.querySelector(".udgivelse").textContent = `Udgivelsesår: ${sang.udgivelsesår}`;
     document.querySelector(".land").textContent = `Land: ${sang.land}`;
     document.querySelector(".genre").textContent = `Genre: ${sang.genre}`;
-    document.querySelector("button").addEventListener("click", tilbage);
+    document.querySelector("button").addEventListener("click", tilbage); //Lytter efter klik på tilbage knap og kalder funktion tilbage
 }
 
+//Går et step tilbage i historikken dvs. tilbage til oversigten
 function tilbage() {
     history.back();
 }
